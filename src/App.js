15710215e65efb16b962eda8502775cfec2703a1
unknown <oibeka50@gmail.com>
2023-07-11 13:59:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [regist, setRegist] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleRegister = () => {
+    if (firstName !== '' && lastName !== '' && email !== '') {
+      setRegist('Success! Thank you for registering');
+    } 
+
+
+    // setRegist('Success! Thank you for registering');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="div_regist">{regist}</div>
+      <input
+        type="text"
+        placeholder="first name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <br />
+      <input
+        type="text"
+        placeholder="last name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <br />
+      <input
+        type="email"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <button
+        className="but_regist"
+        type="submit"
+        onClick={handleRegister}
+      >
+        register
+      </button>
     </div>
   );
 }
